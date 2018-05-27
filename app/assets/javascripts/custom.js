4aacca84be0e0dbd6d8f22d85a1d9fb7eab91285
Personal <personal@Alvaros-MacBook-Pro.local>
2018-05-27 19:41:24 +0100

    Footer login/logout

---
+++ app/assets/javascripts/custom.js
@@ -0,0 +1,8 @@
+// without jQuery (doesn't work in older IEs)
+document.addEventListener('DOMContentLoaded', function(){ 
+	
+	//Tooltip Function Boostrap 4
+	$('[data-toggle="tooltip"]').tooltip();
+
+
+}, false);
